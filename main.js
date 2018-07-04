a53e672efe1c28e12adfb043063fa89c5cdcf216
Luke Camelo <azathothbeats@gmail.com>
2018-07-04 18:10:49 -0400

    Added comments to javascript file

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 let skills = document.querySelectorAll('.tech')
 
+// ensure that tech list/skills fade in the same on desktop as they do mobile
 if (window.matchMedia("(max-width: 950px)").matches) {
 
   for(let i = 0; i < skills.length; i++) {
@@ -35,13 +36,14 @@ if (window.matchMedia("(max-width: 950px)").matches) {
   
 }
 
+// fade in elements on page
 window.sr = ScrollReveal()
 sr.reveal('#project')
 sr.reveal('#contact')
 sr.reveal('.tech', 50)
 sr.reveal('.techMobile')
 
-
+// initialize particles.js header
 particlesJS('particles-js', 
 {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"bubble"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true}, function() {
   console.log('particles.js activate!')
